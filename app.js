1de809bf1186d2746d1daaf3eb7db4fd3cb8bf99
Ankit Shankar <ankitshankar@gmail.com>
2012-02-07 01:59:20 -0500

    app.js cleanup

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
+var coffee = require('coffee-script');
+var express = require('express');
+var routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
+app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -19,11 +15,11 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
   app.use(express.errorHandler()); 
 });
 
